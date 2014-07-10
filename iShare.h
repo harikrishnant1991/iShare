ba02ec2882e40cc9075d62ea53f6b3964169563b
Harikrishnan T <harikrishnant1991@yahoo.com>
2014-07-10 15:29:24 +0530

    Create iShare.h

---
+++ iShare.h
@@ -0,0 +1,23 @@
+//
+//  iShare.h
+//  iShare
+//
+//  Created by Harikrishnan T on 10/07/14.
+//  Copyright (c) 2014 FyrWeel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MessageUI/MessageUI.h>
+#import <MessageUI/MFMailComposeViewController.h>
+#import <Social/Social.h>
+#import "AppDelegate.h"
+
+@interface iShare : UIActionSheet<UIActionSheetDelegate>
+{
+  NSString *shareContent;
+  UIImage *shareImage;
+}
+
+- (void) setShareContent:(NSString *)content withImage:(UIImage *)image;
+
+@end
